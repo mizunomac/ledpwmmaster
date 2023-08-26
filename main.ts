779454cbbe336f3d3038648b50a1448cf74858de
mizunomac <97015996+mizunomac@users.noreply.github.com>
2023-08-26 16:28:20 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.pause(100)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    basic.showIcon(IconNames.Square)
+    basic.pause(100)
+    basic.clearScreen()
 })
 radio.setGroup(123)
 basic.forever(function () {
